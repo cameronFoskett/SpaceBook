34b8128b8e369e913dd47211731d4e77a7f19773
Cameron Foskett <camfosk@Camerons-MBP.lan>
2022-02-01 21:11:08 +0000

    added a new create account page

--- App.js
+++ App.js
@@ -1,15 +1,56 @@
 import React from 'react';
 import { View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import Login from './screens/Login.js';
+import CreateAccount from './screens/CreateAccount.js';
+import Profile from './screens/Profile.js';
+import Friends from './screens/Friends.js';
+
 import Tabs from './navigation/tabs';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const Auth = () => {
   return (
-    <>
-      <Login />
-    </>
-  )
-}
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="CreateAccount"
+        component={CreateAccount}
+        options={{
+          title: 'Create Account', 
+          headerStyle: {
+            backgroundColor: '#307ecc', //Set Header color
+          },
+          headerTintColor: '#fff', //Set Header text color
+          headerTitleStyle: {
+            fontWeight: 'bold', //Set Header text style
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Auth"
+          component={Auth}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
 
--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -1,31 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory,
+} from "react-router-dom";
 import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
 
 import spaceBookLogo from '../assets/SpaceBook-logos.jpeg';
-import CreateAccount from '../screens/CreateAccount.js';
 
-const Login = ({navigation}) => {
+export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [token, setToken] = useState('');
-  const [error, setError] = useState('');
+  const [firstname, setFirstname] = useState('');
+  const [surname, setSurname] = useState('');
 
+  const [error, setError] = useState('');
+//   const [userData, setUserData] = useState(
+//     {
+//         email:'', 
+//         password:'', 
+//         firstname:'', 
+//         surname:''
+//     });
 
   async function handleLogin(){
-    let history = useHistory();
 
-    const response = await fetch("http://localhost:3333/api/1.0.0/login",
+    const response = await fetch("http://localhost:3333/api/1.0.0/user",
     {
       method: 'POST',
       headers: { 'Content-Type': 'application/json'},
       body: JSON.stringify({
+        first_name: firstname,
+        last_name: surname,
         email: email,
         password: password
       })
     })
     .catch((error) => {
       console.error("error");
+      setError(error);
     });
     const data = await response.json();
     console.log(data);
@@ -33,45 +49,56 @@ const Login = ({navigation}) => {
         setError('An error occured please try again...');
     }
     else {
-        setToken(data.token);
         return response;
     }
   }
 
   return (
     <>
-    <View style={styles.container}>
+      <View style={styles.container}>
         <Image source={spaceBookLogo} style={styles.image}/>
         <StatusBar style="auto" />
         <View style={styles.inputView}>
-            <TextInput
-                style={styles.TextInput}
-                placeholder="Enter email"
-                placeholderTextColor="#003f5c"
-                onChangeText={(email) => setEmail(email)}
-            />
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Enter your firstname."
+            placeholderTextColor="#003f5c"
+            onChangeText={(firstname) => setFirstname(firstname)}
+          />
+        </View> 
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Enter your surname."
+            placeholderTextColor="#003f5c"
+            onChangeText={(surname) => setSurname(surname)}
+          />
+        </View> 
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Enter your email"
+            placeholderTextColor="#003f5c"
+            onChangeText={(email) => setEmail(email)}
+          />
         </View>
         <View style={styles.inputView}>
-            <TextInput
-                style={styles.TextInput}
-                placeholder="Enter password."
-                placeholderTextColor="#003f5c"
-                secureTextEntry={true}
-                onChangeText={(password) => setPassword(password)}
-            />
-        </View> 
-        <TouchableOpacity onPress={()=>navigation.navigate('CreateAccount')}>
-            <Text style={styles.newAccount}>Don't have an account?</Text>
-        </TouchableOpacity> 
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Enter your password."
+            placeholderTextColor="#003f5c"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          />
+        </View>  
+        {error && <Text>{error}</Text>}
         <TouchableOpacity style={styles.loginBtn} onPress={handleLogin}>
-            <Text style={styles.loginText}>LOGIN</Text>
+          <Text style={styles.loginText}>SIGN UP</Text>
         </TouchableOpacity> 
-    </View>
+      </View>
     </>
   );
 }
-
-export default Login;
 
 const styles = StyleSheet.create({
   header: {
@@ -84,10 +111,6 @@ const styles = StyleSheet.create({
     width:'33%',
     height:'15%',
     borderRadius: '50%',
-  },
-  forgot_button: {
-    height: 30,
-    marginBottom: 30,
   },
    inputView: {
     backgroundColor: '#518abc',
